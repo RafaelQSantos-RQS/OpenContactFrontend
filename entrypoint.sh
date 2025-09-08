b52e6011546f0cffe82362beed99ae1d45e672ea
RafaelQsantos-RQS <rafaelqsantos33@gmail.com>
2025-09-07 23:46:53 -0300

    feat: Configure Nginx with backend API URL env var
    - Configure Nginx to use the backend API URL environment variable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,7 @@ set -e
 
 NGINX_CONF="/etc/nginx/nginx.conf"
 
-# Faz substituição in-place usando um arquivo temporário
 envsubst '${BACKEND_API_URL}' < "$NGINX_CONF" > "$NGINX_CONF.tmp" \
   && mv "$NGINX_CONF.tmp" "$NGINX_CONF"
 
-# Start Nginx
 exec nginx -c "$NGINX_CONF" -g 'daemon off;'
